9846d8575789534df154d249d1172e60fc0d1922
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-03-04 11:13:18 +0100

    fixed little issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 require('coffee-script').register();
-SortRecursive = require('./lib/SortRecursive');
+var SortRecursive = require('./lib/SortRecursive');
 
 module.exports = function (obj, compareFn) {
-  return SortRecursive.object(obj);
-}
+  return SortRecursive.object(obj, compareFn);
+};
